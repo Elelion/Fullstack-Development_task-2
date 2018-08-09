583212c625e0ee6066c83e8dcc58445dfb75c100
unknown <elelion@yandex.ru>
2018-08-09 16:53:03 +0300

    landing page

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<HTML>
+
+<HEAD> 
+	<!-- SEO -->
+	<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+	<meta name="Keywords" content="Fullstack Development, Fullstack, Development">
+	<link rel = "shortcut icon" type = "image/x-icon" href = "images/favicon.png"> 
+	<title> Quest-1, maket-2 </title>
+
+	<!-- CSS's -->  
+	<link rel="stylesheet/less" href="css/styles.css">        	  
+	<script src="lib/less.min.js"></script>	
+
+	<!-- func -->
+	<?php /*require_once 'src/functions.php';*/ ?>
+</HEAD>
+
+<BODY>
+<header>
+	<div class="header__standard">
+		<div class="header__standard--title">
+			<p class="titleFont">standard buttons</p>
+		</div>
+
+		<div class="header__standard--buttonsBlock">	
+			<div class="header__standard--row small">				
+				<div class="header__standard--block small__buttons">
+					<button class="small-button animate green">Button</button>
+				</div>
+
+				<div class="header__standard--block small__buttons">
+					<button class="small-button animate green">Button</button>
+				</div>
+
+				<div class="header__standard--block small__buttons">
+					<button class="small-button animate green">Button</button>
+				</div>
+			</div>
+
+			<div class="header__standard--row normal">
+				<div class="header__standard--block">
+					<button class="standard-button animate green">Button</button>
+				</div>
+
+				<div class="header__standard--block">
+					<button class="standard-button animate green">Button</button>
+				</div>
+
+				<div class="header__standard--block">
+					<button class="standard-button animate green">Button</button>
+				</div>
+			</div>
+
+			<div class="header__standard--row normal second">
+				<div class="header__standard--block">
+					<button class="standard-button animate red rightFontColor">Button</button>
+				</div>
+
+				<div class="header__standard--block">
+					<button class="standard-button animate red rightFontColor">Button</button>
+				</div>
+
+				<div class="header__standard--block">
+					<button class="standard-button animate red rightFontColor">Button</button>
+				</div>
+			</div>
+
+			<div class="header__standard--row small">
+				<div class="header__standard--block small__buttons">
+					<button class="small-button animate red rightFontColor">Button</button>
+				</div>
+
+				<div class="header__standard--block small__buttons">
+					<button class="small-button animate red rightFontColor">Button</button>
+				</div>
+
+				<div class="header__standard--block small__buttons">
+					<button class="small-button animate red rightFontColor">Button</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="header__arrows">
+		<div class="header__arrows--title">
+			<p class="titleFont">arrow buttons</p>
+		</div>
+
+		<div class="header__arrows--arrowsBlock">	
+			arrows...
+
+			<!-- 
+			<form action="">
+				<button class="small-button animate green ripple">Button<span></span></button>
+			</form> 
+			-->
+		</div>
+	</div>
+</header>
+
+<!-- .... -->
+
+<main>
+	<div class="main__charts">		
+		<p>MAIN: main__charts</p>		
+	</div>
+
+	<div class="main__sliders">
+		<p>MAIN: main__sliders</p>
+	</div>
+
+	<div class="main__stages">
+		<p>MAIN: main__stages</p>
+	</div>
+
+	<div class="main__elements">
+		<p>MAIN: main__elements</p>
+	</div>
+
+	<div class="main__blogs">
+		<p>MAIN: main__blogs</p>
+	</div>
+
+	<div class="main__news">
+		<p>MAIN: main__news</p>
+	</div>
+</main>		
+
+<!-- .... -->
+
+<footer>
+	<div class="footer__location">
+		<p>MAIN: footer__location</p>
+	</div>
+
+	<div class="footer__blog">
+		<p>MAIN: footer__blog</p>
+	</div>
+
+	<div class="footer__video">
+		<p>MAIN: footer__video</p>
+	</div>
+</footer>
+
+</BODY>
+</HTML>
+
+<script src="src/ripple.js"></script>
